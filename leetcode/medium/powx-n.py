574b09c9301e15d751809e1070132c6ade0dcd07
Joonghyun Lee <jhlee3141592@gmail.com>
2019-06-25 00:25:49 +0900

    modify 50 in leetcode

--- leetcode/medium/powx-n.py
+++ leetcode/medium/powx-n.py
@@ -9,11 +9,11 @@ class Solution(object):
             if n == 0:
                 return 1
             elif n < 0:
-                return 1 / pow(x, -n)
+                return 1 / helper(x, -n)
             elif n % 2 == 1:
-                return x * pow(x, n - 1)
+                return x * helper(x, n - 1)
 
-            return pow(x * x, n // 2)
+            return helper(x * x, n // 2)
 
         return helper(x, n)
 
@@ -25,4 +25,6 @@ if __name__ == '__main__':
     r = s.myPow(2.1, 3)
     print(r)
     r = s.myPow(2.0, -2)
+    print(r)
+    r = s.myPow(2.0, -2147483648)
     print(r)
